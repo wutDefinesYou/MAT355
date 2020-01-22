5fbbd49178dfecd366eff2af3380fd60b49d3509
liz <liz@augsburg.edu>
2020-01-22 10:49:10 -0600

    added file

--- lecture2/Horner.m
+++ lecture2/Horner.m
@@ -1,12 +1,26 @@
 function [y q] = Horner(a,r)
 
-n = numel(a);
-q = zeros(1,n);
-q(1) = a(1);
+%% Horner's algorithm
+%% Nested multiplication form to compute polynomials
+%%
+%% Inputs:
+%%    a: array of coefficients of the polynomial, in descending order of exponent
+%%    r: value at which the polynomial is being evaluated
+%%
+%% Outputs:
+%%    y: value of the polynomial
+%%    q: array of coefficients of the deflated polynomial
+%% Usage:
+%%    [y q] = Horner(a,r);
+%%    The first output would be y, and the next would be q
 
-for 2:n
-  q(i) = a(i) + r * q(i-1);
+n = numel(a);   % the number of coefficients
+q = zeros(1,n-1);   % initialize vector q
+q(1) = a(1);  % let the first term in q equals the first term in a
+
+for i = 2:n-1
+  q(i) = a(i) + r * q(i-1);   % compute each term of q
 endfor
 
-y = q(n);
+y = a(n) + r * q(n-1);  % evaluate the last term in q & the value of polynomial
 endfunction
